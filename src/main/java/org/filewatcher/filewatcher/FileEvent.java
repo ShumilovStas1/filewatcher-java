71b018d0706287a2e6ee1d934432fbe96e03968c
Stanislav Shumilov <stanislav.shumilov@sectigo.com>
2025-12-19 18:47:54 +0200

    removed toString for FileEvent

--- src/main/java/org/filewatcher/filewatcher/FileEvent.java
+++ src/main/java/org/filewatcher/filewatcher/FileEvent.java
@@ -3,16 +3,6 @@ package org.filewatcher.filewatcher;
 import java.nio.file.Path;
 
 public record FileEvent(Type type, Path target, boolean isDir, Path rootDir) {
-
-    @Override
-    public String toString() {
-        return "FileEvent{" +
-               "type=" + type +
-               ", target=" + target +
-               ", isDir=" + isDir +
-               ", rootDir=" + rootDir +
-               '}';
-    }
 }
 
 
